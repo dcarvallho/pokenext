7e8504f70458fd3b71b7bb59f2d4e47c9c5b0354
Davi Augusto <davi_augusto11@hotmail.com>
2023-07-28 00:24:49 -0300

    Added pokemons card and adjustments in README

--- components/card/index.js
+++ components/card/index.js
@@ -1,16 +1,21 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
+
+import style from './style.module.css'
 
 const Card = ({ pokemon }) => {
   return (
-    <div>
+    <div className={style.card}>
       <Image
         src={`https://cdn.traction.one/pokedex/pokemon/${pokemon.id}.png`}
         width="120"
         height="120"
         alt={pokemon.name}
       />
+      <p>{pokemon.id}</p>
       <h3>{pokemon.name}</h3>
+      <Link href={`/pokemon/${pokemon.id}`}>details</Link>
     </div>
   );
 };
